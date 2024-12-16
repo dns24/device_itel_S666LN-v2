67788194a10c42d9763b612b744da4a143d2db85
dns24 <wahidns24@gmail.com>
2024-12-16 00:08:35 +0000

    S666LN: attempt to fix offline charging...
    ... and fast charging

--- device.mk
+++ device.mk
@@ -188,9 +188,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.mediatek \
-    android.hardware.health-service.mediatek-recovery \
-    charger_res_images_vendor
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
